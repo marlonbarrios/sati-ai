dd8111a4ec2c8b1aa4d02fe15ba1b023e03ad9e0
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-16 22:38:53 -0400

    Update ChatLine.tsx

--- components/ChatLine.tsx
+++ components/ChatLine.tsx
@@ -18,7 +18,7 @@ export const LoadingChatLine = () => (
       <div className="min-w-0 flex-1">
         <p className="font-large text-xxl text-gray-900">
           <a href="#" className="hover:underline">
-            AI
+            Bhikkh-AI
           </a>
         </p>
         <div className="space-y-4 pt-4">
@@ -60,7 +60,7 @@ export function ChatLine({ role = 'assistant', content }: ChatGPTMessage) {
             <div className="flex-1 gap-4">
               <p className="font-large text-xxl text-gray-900">
                 <a href="#" className="hover:underline">
-                  {role == 'assistant' ? 'AI' : 'You'}
+                  {role == 'assistant' ? 'Bhikkh-AI' : 'You'}
                 </a>
               </p>
               <p
