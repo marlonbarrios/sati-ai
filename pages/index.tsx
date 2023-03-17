688cfd4664be1f3e6149cdba18241a0696e5ab9d
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-17 14:25:23 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,11 @@ function Home() {
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
         <Text variant="h1">Bhikkh-AI</Text>
-        <Text className="text-zinc-600">
-         Introducing Bhikkh-AI, an AI-powered Buddhist meditation guide that can answer all your questions about mindfulness meditation 
-          and early Buddhist concepts and philosophy. This innovative tool is <b>multilingual</b>, so you can receive guidance on your meditation 
-          practice in various languages. Simply ask anything related to mindfulness and early Buddhism, and the chatbot will provide you with wise and reliable guidance. Experience the benefits of having a meditation guide right at your fingertips, wherever and whenever you need it.
-        </Text>
+        <Text className="text-zinc-600">Introducing Bhikkh-AI, an AI-powered Buddhist meditation guide that can answer all your questions about mindfulness meditation and early Buddhist concepts and philosophy. This innovative tool is <b>multilingual</b>, so you can receive guidance on your meditation 
+practice in various languages. Simply ask anything related to mindfulness and early Buddhism, and the chatbot will provide you with wise and reliable guidance. Experience the benefits of having a meditation guide right at your fingertips, wherever and whenever you need it.</Text>
          <Text className="text-zinc-600"><i>Bhikkh-AI, powered by a large AI language model, aims to support your meditation practice but is not meant to replace knowledgeable human teachers, friends, or mental health professionals.</i></Text>
          <Text className="text-zinc-600"><i>Bhikkh-AI is envisioned as a digital art project, and like anything else, it should be approached with an open yet critical mind. It may have inaccuracies and may occasionally respond in unexpected ways.</i></Text>
-          <Text className="text-zinc-600"> <b>If any of my previous responses seemed incomplete, please type "continue," and I will do my best to expand or clarify my previous statements.</b></Text>
+          <Text className="text-zinc-600">If any of my previous responses seemed incomplete, please type "continue," and I will do my best to expand or clarify my previous statements.</Text>
        
       </section>
       <section className="flex flex-col gap-3">
