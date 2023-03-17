c1c6414a1903abd1fc6c2f5cf7f457b4e848b4eb
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-17 19:43:50 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import type { LayoutProps } from '@vercel/examples-ui/layout'
 
+import { getLayout } from '@vercel/examples-ui'
 
 import '@vercel/examples-ui/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-
+  const Layout = getLayout<LayoutProps>(Component)
 
   return (
-  
+    <Layout
+     
+    >
       <Component {...pageProps} />
       <Analytics />
-  
+    </Layout>
   )
 }
 
 export default App
+
