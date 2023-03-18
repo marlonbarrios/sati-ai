da9f53b6a333a935235478031a000d88a4befb96
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-18 10:09:41 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import type { LayoutProps } from '@vercel/examples-ui/layout';
 import {Home} from 'Home';
-import Head from 'next/head'
+import type  {Head}from 'next/head'
 
 import { getLayout } from '@vercel/examples-ui';
 
