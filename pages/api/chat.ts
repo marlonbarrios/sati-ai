22c3f36cc93cc6a97437ace539fd5a57e56145ad
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-17 21:27:41 -0400

    Update chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -31,7 +31,7 @@ const handler = async (req: Request): Promise<Response> => {
   }
 
   const payload: OpenAIStreamPayload = {
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4',
     messages: messages,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS
