cb30e45240cea5c369bd812773c36ecae5b560c0
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-17 21:52:59 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ function Home() {
         <Text className="text-zinc-600">Introducing Bhikkh-AI, an AI-powered Buddhist meditation guide that can answer all your questions about mindfulness meditation and early Buddhist concepts and philosophy. This innovative tool is <b>multilingual</b>, so you can receive guidance on your meditation 
 practice in various languages. Simply ask anything related to mindfulness and early Buddhism, and the chatbot will provide you with wise and reliable guidance. Experience the benefits of having a meditation guide right at your fingertips, wherever and whenever you need it.</Text>
          <Text className="text-zinc-600"><i>Bhikkh-AI, powered by a large AI language model, aims to support your meditation practice but is not meant to replace knowledgeable human teachers, friends, or mental health professionals.</i></Text>
+        
          <Text className="text-zinc-600"><i>Bhikkh-AI is envisioned as a digital art project, and like anything else, it should be approached with an open yet critical mind. It may have inaccuracies and may occasionally respond in unexpected ways.</i></Text>
        
          
@@ -24,7 +25,9 @@ practice in various languages. Simply ask anything related to mindfulness and ea
         </div>
         <Text className="text-zinc-600"><b>This project is dedicated with respect and gratitude to all my teachers</b></Text>
      <Text className="text-zinc-600">Concept, programming and prompt engineering by <Link href="https://marlonbarrios.github.io/">Marlon Barrios Solano</Link></Text>
-           <Text className="text-zinc-600"> <Link href="https://patreon.com/user?u=84024387&utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_linkv">Support my research and help with costs of OpenAI</Link></Text>
+         <Text className="text-zinc-600">Powered by <Link href="https://openai.com/product/gpt-4">GPT-4/OpenAI</Link></Text>
+        
+           <Text className="text-zinc-600"> <Link href="https://patreon.com/user?u=84024387&utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_linkv">Support my work in Patreon</Link></Text>
       </section>
     </Page>
   )
