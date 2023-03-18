76b12c7da5e2888198191e702b8008106548ff1d
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-18 08:58:44 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,22 @@
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import type { LayoutProps } from '@vercel/examples-ui/layout'
+import ( Home} from 'Home'
 
 import { getLayout } from '@vercel/examples-ui'
 
 import '@vercel/examples-ui/globals.css'
 
+
 function App({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
+ 
 
   return (
-    <Layout
-      title="ai-chatgpt"
-      path="solutions/ai-chatgpt"
-      description="ai-chatgpt"
+    <Home
     >
       <Component {...pageProps} />
       <Analytics />
-    </Layout>
+    </Home>
   )
 }
 
