446ac3ccc2acd4cb82aa9e0ca638f4f114866626
Marlon Barrios Solano <90220317+marlonbarrios@users.noreply.github.com>
2023-03-18 10:01:47 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import type { LayoutProps } from '@vercel/examples-ui/layout';
 import {Home} from 'Home';
+import Head from 'next/head'
 
 import { getLayout } from '@vercel/examples-ui';
 
@@ -10,6 +11,9 @@ import '@vercel/examples-ui/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
+    <Head>
+        <title>Bhikkh-AI</title>
+      </Head>
     <Home>
       <Component {...pageProps} />
       <Analytics />
